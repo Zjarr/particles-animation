7f6ae8299386eb3a6af098aa92c10caa35179565
Pablo Navarro <j.pablonavarro95@gmail.com>
2023-04-10 18:52:27 -0600

    Tweak params

--- src/utilities.js
+++ src/utilities.js
@@ -1,17 +1,17 @@
 const particleAmount = 100;
 
-const particleMinDirection = -1;
-const particleMaxDirection = 1;
+const particleMinDirection = -2;
+const particleMaxDirection = 2;
 
 const particleMinSize = 2;
 const particleMaxSize = 4;
 
 const particleColor = 'rgba(255, 255, 255, 1)';
 
-const lineMaxSize = 250;
+const lineMaxSize = 200;
 
 const getRandomNumber = (min, max) => {
-  return Math.random() * (max - min + 1) + min;
+  return Math.random() * (max - min) + min;
 }
 
 const loop = (times, callback) => {
